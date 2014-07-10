75ca9b95f2a7647ec40395c7f7d7160dd22f575d
Chris Eisenbraun <chris@chriseisenbraun.com>
2014-07-09 17:56:55 -0700

    Add url and view for about

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Rango says hello world")
+
+
+def about(request):
+    return HttpResponse("Here is the about page")
